f0591c85d57e58f4dc3c0fae6b1e3991f4b2a38f
Verityj <verityj@IT-Admins-Mac-mini.local>
2018-06-15 16:01:19 -0400

    database error

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -26,21 +26,18 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     let username = Environment.get("DATABASE_USER") ?? "vapor"
     let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
     let password = Environment.get("DATABASE_PASSWORD") ?? "password"
-
-    let databasesConfig = PostgreSQLDatabaseConfig(
+    let databaseConfig = PostgreSQLDatabaseConfig(
         hostname: hostname,
         username: username,
         database: databaseName,
         password: password)
-    let database = PostgreSQLDatabase(config: databasesConfig)
+    let database = PostgreSQLDatabase(config: databaseConfig)
     databases.add(database: database, as: .psql)
     services.register(databases)
 
     /// Configure migrations
     var migrations = MigrationConfig()
-    
     migrations.add(model: Acronym.self, database: .psql)
-    
     services.register(migrations)
 
 }
