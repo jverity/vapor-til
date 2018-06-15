6b88b055986c3202203669f56f2a7050df14a0d8
Verityj <verityj@IT-Admins-Mac-mini.local>
2018-06-15 18:35:41 -0400

    Spupdate

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
 
-        // 🔵 Swift ORM (queries, models, relations, etc) built on SQLite 3.
+        // 🔵 Swift ORM (queries, models, relations, etc) built on PostgreSQL.
         .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc")
     ],
     targets: [
--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -21,13 +21,13 @@ public func configure(_ config: inout Config, _ env: inout Environment, _ servic
     
     /// Register the configured database to the database config.
     var databases = DatabasesConfig()
-    
     let hostname = Environment.get("DATABASE_HOSTNAME") ?? "10.0.77.37"
     let username = Environment.get("DATABASE_USER") ?? "vapor"
     let databaseName = Environment.get("DATABASE_DB") ?? "vapor"
     let password = Environment.get("DATABASE_PASSWORD") ?? "password"
     let databaseConfig = PostgreSQLDatabaseConfig(
         hostname: hostname,
+        port: 5432,
         username: username,
         database: databaseName,
         password: password)
